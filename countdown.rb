a2ac71d97c0e7985c377aa0555652335b283d915
Daniel Calvin <arockne2003@yahoo.com>
2021-09-29 13:17:12 -0700

    commit tree f1579b1e544ff8126c895fafd80d4cc20f9be3c1

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
-#write your code here
+require 'pry'
 
-def countdown
+#define countdown
+  #takes in integar
+  #uses while loop
+  #countdown from int to one
+  #should return "HAPPY NEW YEAR!"
+def countdown(count)
+  while(count >= 1) do
+    puts "#{count} SECOND(S)!"
+    count -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(count)
+  countdown(count)
 end
